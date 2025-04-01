53ed2aa66fbbc4f4bd372eb2575741cc1eb923de
AthThobari <thobari.mutu@gmail.com>
2025-04-01 17:05:52 +0700

    feat(auth): implement login API using JWT

--- app/controllers/user_controller.go
+++ app/controllers/user_controller.go
@@ -2,15 +2,17 @@ package controllers
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/kooroshh/fiber-boostrap/app/models"
 	"github.com/kooroshh/fiber-boostrap/app/repository"
+	"github.com/kooroshh/fiber-boostrap/pkg/jwt_token"
 	"github.com/kooroshh/fiber-boostrap/pkg/response"
 	"golang.org/x/crypto/bcrypt"
 )
 
-func Register(ctx *fiber.Ctx) error{
+func Register(ctx *fiber.Ctx) error {
 	user := new(models.User)
 
 	err := ctx.BodyParser(user)
@@ -43,5 +45,68 @@ func Register(ctx *fiber.Ctx) error{
 
 	resp := user
 	resp.Password = ""
+	return response.SendsuccessResponse(ctx, resp)
+}
+
+func Login(ctx *fiber.Ctx) error {
+	loginReq := new(models.LoginRequest)
+	resp := models.LoginResponse{}
+
+	err := ctx.BodyParser(loginReq)
+	if err != nil {
+		errResponse := fmt.Errorf("failed to parse request: %v", err)
+		fmt.Println(errResponse)
+		return response.SendFailureResponse(ctx, fiber.StatusBadRequest, errResponse.Error(), nil)
+	}
+	err = loginReq.Validate()
+	if err != nil {
+		errResponse := fmt.Errorf("failed to validate request: %v", err)
+		fmt.Println(errResponse)
+		return response.SendFailureResponse(ctx, fiber.StatusBadRequest, errResponse.Error(), nil)
+	}
+
+	user, err := repository.GetUserByUsername(ctx.Context(), loginReq.Username)
+	if err != nil {
+		errResponse := fmt.Errorf("failed to get username: %v", err)
+		fmt.Println(errResponse)
+		return response.SendFailureResponse(ctx, fiber.StatusNotFound, "email/password incorrect", nil)
+	}
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(loginReq.Password))
+	if err != nil {
+		errResponse := fmt.Errorf("failed to get username: %v", err)
+		fmt.Println(errResponse)
+		return response.SendFailureResponse(ctx, fiber.StatusNotFound, "email/password incorrect", nil)
+	}
+
+	token, err := jwt_token.GenerateToken(ctx.Context(), user.Username, user.Fullname, "token")
+	if err != nil {
+		errResponse := fmt.Errorf("failed to generate token: %v", err)
+		fmt.Println(errResponse)
+		return response.SendFailureResponse(ctx, fiber.StatusNotFound, "terjadi kesalahan pada sistem", nil)
+	}
+	refreshToken, err := jwt_token.GenerateToken(ctx.Context(), user.Username, user.Fullname, "refresh_token")
+	if err != nil {
+		errResponse := fmt.Errorf("failed to generate token: %v", err)
+		fmt.Println(errResponse)
+		return response.SendFailureResponse(ctx, fiber.StatusNotFound, "terjadi kesalahan pada sistem", nil)
+	}
+
+	err = repository.InsertNewUserSession(ctx.Context(), &models.UserSession{
+		CreatedAt:    time.Now(),
+		UpdatedAt:    time.Now(),
+		UserID:       user.ID,
+		Token:        token,
+		RefreshToken: refreshToken,
+		TokenExpired: time.Now().Add(time.Hour * 3),
+		RefreshTokenExpired: time.Now().Add(time.Hour * 72),
+	})
+
+	if err != nil {
+	}
+
+	resp.Username = user.Username
+	resp.Fullname = user.Fullname
+	resp.Token = token
+	resp.RefreshToken = refreshToken
 	return response.SendsuccessResponse(ctx, resp)
 }
---
+++ pkg/jwt_token/jwt.go
@@ -0,0 +1,69 @@
+package jwt_token
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/kooroshh/fiber-boostrap/pkg/env"
+)
+
+type ClaimToken struct {
+	Username string `json:"username"`
+	Fullname string `json:"fullname"`
+	jwt.RegisteredClaims
+}
+
+var mapTypeToken = map[string]time.Duration{
+	"token": time.Hour * 3,
+	"refres_token": time.Hour * 72,
+}
+
+var jwtSecret = []byte(env.GetEnv("APP_SECRET", ""))
+
+func GenerateToken(ctx context.Context, username, fullname, tokenType string) (string, error) {
+	secret := []byte(env.GetEnv("APP_SECRET", ""))
+
+	claimToken := ClaimToken{
+		Username: username,
+		Fullname: fullname,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer: env.GetEnv("APP_NAME", ""),
+			IssuedAt: jwt.NewNumericDate(time.Now()),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(mapTypeToken[tokenType])),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claimToken)
+
+	resultToken, err :=  token.SignedString(secret)
+	if err != nil {
+		return resultToken, fmt.Errorf("failed to generate token %v", err)
+	}
+	return resultToken, nil
+}
+
+func ValidateToken(ctx context.Context, token string) (*ClaimToken, error) {
+	var (
+		claimToken *ClaimToken
+		ok bool
+	)
+
+	jwtToken, err := jwt.ParseWithClaims(token, &ClaimToken{}, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("failed to validate method jwt: %v", t.Header["alg"])
+		}
+		return jwtSecret, nil
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse jwt: %v", err)
+	}
+
+	if claimToken, ok = jwtToken.Claims.(*ClaimToken); !ok || !jwtToken.Valid {
+		return nil, fmt.Errorf("token invalid")
+	}
+
+	return claimToken, nil
+}
